bafa1e7537b826c9d15cbb0de46b3451a66f4151
faridAbinah <faridabinah@hotmail.com>
2021-11-10 15:41:46 -0500

    Fixed bug with dollar sign not appearing in the cart and the show pages for products.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,12 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    
     @products = @category.products.order(created_at: :desc)
   end
 
 end
+
+# raise @category.inspect Is good when checking whats inside a instance variable
+# We can use this in our erb files as well like this <%= raise @category %>
+
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,19 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    
   end
 
   def show
     @product = Product.find params[:id]
+   
   end
 
 end
+
+# Instance variables are automatically available to the view that the action is rendering.
+# The index action for the products controller will automatically render the index view located in views/products/index.html.erb
+# Same thing goes for the show it will automatically render the show views/products/show.html.erb
+# Instance variables are transferred from the controller into the view automatically.
+# @msg = 'Hello Farid' if this was declared in our show action we could go to our show view and do <%= @msg %> and it will display the message
+# This is because any instance variable declared in the action will automatically be available for use in the view
