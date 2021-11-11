7d90f3a166669357787446d35a95ae8c950e3ce9
faridAbinah <faridabinah@hotmail.com>
2021-11-10 20:36:40 -0500

    Added functionality to display the number of products and categories in the store

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @products = Product.all.count
+    @categories = Category.all.count 
+    # raise @categories.inspect
   end
 end
